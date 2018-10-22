f1cd6c92754338cc58b65ba5e98c2bf960876a87
Tomi Karikj <karikjtomi@gmail.com>
2018-10-22 22:08:04 +0200

    Finished oauth flow

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
 require('./services/passport');
 const chalk = require('chalk');
 
+mongoose.connect(
+  keys.mongoURI,
+  { useNewUrlParser: true }
+);
+
 const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(chalk.cyan('The server has successfully connected...'));
+  console.log(chalk.cyan(`Server is listening on port ${PORT}`)),
+    console.log(chalk.yellow('Press CTRL-C to stop'));
 });
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +23,17 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token: ', accessToken);
-      console.log('refresh token: ', refreshToken);
-      console.log('profile: ', profile);
+      User.findOne({ googleID: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // Vekje postoi registriran clen so google profile id
+          done(null, existingUser);
+        } else {
+          // Clenot ne postoi, zapisi go vo baza
+          new User({ googleID: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
     }
   )
 );
